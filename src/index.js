50af8c71bf6d858c7967b96d0f9ef11a4c84c38f
jmoran9 <moranljessica@gmail.com>
2024-03-22 15:08:06 -0400

    Added icon weather data

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-
-
+    let iconElement = document.querySelector("#icon");
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
@@ -16,6 +15,8 @@ function refreshWeather(response) {
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} mph`;
     temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class= "weather-app-icon"></img>`;
+
 }
 
 function formatDate(date) {
